442c712a69ca126ddcec0ab69a763e8137775f32
Yan Pershay <yanik22827@gmail.com>
2021-03-07 23:16:03 +0300

    new post screen

--- SportHub_client/lib/screens/newpost/add_newpost_screen.dart
+++ SportHub_client/lib/screens/newpost/add_newpost_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:SportHub_client/utils/file.dart';
 import 'package:flutter/material.dart';
 import 'package:storage_path/storage_path.dart';
@@ -26,18 +25,16 @@ class _NewPostScreenState extends State<NewPostScreen> {
   }
 
   @override
-  void initState() async {
+  void initState() {
     super.initState();
-    var status = await Permission.storage.status;
-    if (!status.isGranted) {
-      await Permission.storage.request();
-    }
+
     getImagesPath();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: new AppBar(
         automaticallyImplyLeading: false,
         backgroundColor: Colors.white,
@@ -52,31 +49,75 @@ class _NewPostScreenState extends State<NewPostScreen> {
                 color: Colors.black,
               ),
               tooltip: 'Show Snackbar',
-              onPressed: () {}),
+              onPressed: () {
+                Navigator.pop(context);
+              }),
         ],
       ),
-      body: Column(
-        children: <Widget>[
-          new TextField(
-            keyboardType: TextInputType.multiline,
-            maxLines: null,
-          ),
-          Row(
-            children: <Widget>[
-              DropdownButtonHideUnderline(
-                  child: DropdownButton(items: getItems(), onChanged: (d) {}))
-            ],
-          ),
-          Divider(),
-          Container(
-              height: MediaQuery.of(context).size.height * 0.45,
-              child: image != null
-                  ? Image.file(File(image),
-                      height: MediaQuery.of(context).size.height * 0.45,
-                      width: MediaQuery.of(context).size.width * 0.45)
-                  : Container()),
-          Divider(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            new TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+            ),
+            Row(
+              children: <Widget>[
+                DropdownButtonHideUnderline(
+                    child: DropdownButton<FileModel>(
+                  items: getItems(),
+                  onChanged: (FileModel d) {
+                    assert(d.files.length > 0);
+                    image = d.files[0];
+                    setState(() {
+                      selectedModel = d;
+                    });
+                  },
+                  value: selectedModel,
+                ))
+              ],
+            ),
+            Divider(),
+            Container(
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: image != null
+                    ? Image.file(File(image),
+                        height: MediaQuery.of(context).size.height * 0.45,
+                        width: MediaQuery.of(context).size.width * 0.45)
+                    : Container()),
+            Divider(),
+            selectedModel == null && selectedModel.files.length < 1
+                ? Container()
+                : Container(
+                    height: MediaQuery.of(context).size.height * 0.4,
+                    child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 4,
+                          crossAxisSpacing: 4,
+                          mainAxisSpacing: 4),
+                      itemBuilder: (_, i) {
+                        var file = selectedModel.files[i];
+                        return GestureDetector(
+                          child: Image.file(
+                            File(file),
+                            fit: BoxFit.cover,
+                          ),
+                          onTap: () {
+                            setState(() {
+                              image = file;
+                            });
+                          },
+                        );
+                      },
+                      itemCount: selectedModel.files.length,
+                    ),
+                  ),
+            ElevatedButton(
+              child: Text('Bottom'),
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
